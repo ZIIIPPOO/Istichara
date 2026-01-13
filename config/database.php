46d24412bb512222181de19b6394b30a33943d87
ZIIIPPOO <youssefboudouar77@gmail.com>
2026-01-13 14:12:27 +0100

    feat: implement datbase class with Singleton pattern

---
+++ config/database.php
@@ -0,0 +1,37 @@
+<?php
+class Database {
+
+    private $connection;
+
+    public $host = "localhost";
+    public $db_name = "istichara";
+    public $username = "root";
+    public $password = "zippo";
+    public static $instance = null;
+
+
+
+    private function __construct()
+    {
+        $this->connection = new PDO(
+            "mysql:host=$this->host;dbname=$this->db_name",
+            $this->username,
+            $this->password
+        );
+
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+    public static function getInstance()
+    {
+        if(self::$instance === null)
+        {
+            self::$instance = new Database();
+        }
+        return self::$instance; 
+    }
+
+}
