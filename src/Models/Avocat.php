78a9bbaddf53b271dece11a69ae6058e4f2f4c50
ZIIIPPOO <youssefboudouar77@gmail.com>
2026-01-14 21:09:34 +0100

    feat: implemeneted avocat class methods

--- src/Models/Avocat.php
+++ src/Models/Avocat.php
@@ -1,13 +1,47 @@
 <?php
-
+require_once 'Person.php';
 class Avocat extends Person
 {
     private $specialites;
     private $consultationEnLigne;
 
-    public function getId(){return $this->specialites;}
+    public function getSpecialites(){return $this->specialites;}
     public function getConsultationEnligne(){return $this->consultationEnLigne;}
 
-    public function setId($specialites){$this->specialites = $specialites;}
+    public function setSpecialites($specialites){$this->specialites = $specialites;}
     public function setConsultationEnligne($consultationEnLigne){$this->consultationEnLigne = $consultationEnLigne;}
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM avocats";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function getById($id)
+    {
+        $sql = "SELECT * FROM avocats WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+    public function create()
+    {
+        $sql = "INSERT INTO avocats(name, city_id, specialites, annees_experience, tarif_horaire, consultation_en_ligne) VALUES (?,?,?,?,?,?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->name, $this->cityId, $this->specialites, $this->anneesExperience, $this->tarifHoraire, $this->consultationEnLigne]);
+    }
+    public function update()
+    {
+        $sql = "UPDATE avocats SET name = ?, city_id = ?, specialites = ?, annees_experience = ?, tarif_horaire = ?, consultation_en_ligne = ? WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->name, $this->cityId, $this->specialites, $this->anneesExperience, $this->tarifHoraire, $this->consultationEnLigne, $this->id]);
+    }
+    public function delete()
+    {
+        $sql = "DELETE FROM avocats WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->id]);
+    }
 }
