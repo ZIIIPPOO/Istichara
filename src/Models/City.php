1bc03fbcb25fe6232c35048406e19166304c6c8a
ZIIIPPOO <youssefboudouar77@gmail.com>
2026-01-18 00:12:34 +0100

    feat: ajout méthodes getIdByName et getNameById dans City

--- src/Models/City.php
+++ src/Models/City.php
@@ -17,4 +17,21 @@ class City
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    public function getIdByName($name)
+    {
+        $sql = "SELECT id FROM cities WHERE name = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$name]);
+        $res = $stmt->fetch();
+        return $res['id'];
+    }
+    
+    public function getNameById($id)
+    {
+        $sql = "SELECT name FROM cities WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        $res = $stmt->fetch();
+        return $res['name'];
+    }
 }
