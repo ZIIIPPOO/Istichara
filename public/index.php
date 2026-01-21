8487d22036f2b2c63592b694c5eaf694b90e3edd
elmakhtarlaarif <mokhtarlaarif54@gmail.com>
2026-01-21 17:33:16 +0100

    commit index file

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ Router::get('stats', 'StatsController', 'index');
 Router::get('toggle_form/create', 'AvocatController', 'createDynamic');
 
 Router::get('emploi', 'ProfessionnelController', 'AjouteEmploi');
-
+Router::get('emploi/edit', 'ProfessionnelController', 'editEmploi');
 
 Router::dispatch();
 
