25ace5980fd5b8aa8cfa9a377a87fd039f3bbee5
elmakhtarlaarif <mokhtarlaarif54@gmail.com>
2026-01-21 17:21:49 +0100

    modifier disponsibilite du professionnel

--- src/Controllers/ProfessionnelController.php
+++ src/Controllers/ProfessionnelController.php
@@ -27,8 +27,8 @@ class ProfessionnelController
                 $disponiblRep->create($disponibl);
             }
         }
-        $dispo = new DisponibilitesRepo();
-        // $dispo->getAllDispo($user_id);
+        $dispos = new DisponibilitesRepo();
+        $dispos = $dispos->getAllDispo(1); //$_SESSION['user_id']
 
 
         require_once __DIR__ . '/../Views/layouts/header.php';
@@ -36,4 +36,37 @@ class ProfessionnelController
         require_once __DIR__ . '/../Views/layouts/footer.php';
     }
 
+    public function editEmploi()
+    {
+        require_once __DIR__ . '/../Models/Disponibilites.php';
+        require_once __DIR__ . '/../Repositories/DisponibilitesRepo.php';
+        if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET)) {
+            $dispoRow_id = $_GET['id'];
+            if ($dispoRow_id) {
+                $dispoRowData = DisponibilitesRepo::getDisById($dispoRow_id);
+            }
+        }
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST)) {
+            $row_id = $_POST['dispoRow_id'];
+            $user_id = $_SESSION['user_id'];
+            $jour_semaine = isset($_POST['jour_semaine']);
+            $heure_debut = isset($_POST['heure_debut']);
+            $heure_fin = isset($_POST['heure_fin']);
+            $Active = isset($_POST['is_active']);
+
+            $edited = new Disponibilites(
+                $user_id,
+                $jour_semaine,
+                $heure_debut,
+                $heure_fin,
+                $Active
+            );
+            $editedDispo = new DisponibilitesRepo();
+            $editedDispo->edit($edited, $row_id);
+        }
+        require_once __DIR__ . '/../Views/layouts/header.php';
+        require_once __DIR__ . '/../Views/professional/editEmploi.php';
+        require_once __DIR__ . '/../Views/layouts/footer.php';
+    }
 }
